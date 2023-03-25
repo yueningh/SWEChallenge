4b175665b27ebaa3f089720c81f373ab317c15d8
Yuening Huang <yueninghuang962@gmail.com>
2023-03-25 17:10:51 -0400

    front end v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import logo from './SWE_Logo_4C.png';
 import './App.css';
+import React from 'react';
 
-function App() {
+function MyComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class ='attendence_form'>
+      <img src={logo} alt='SWE logo' style={{width: '200px'}} />
+      <h1>SWE Attendence Form</h1>
+      <h4>Enter your information below</h4>
+      <form class = 'form'>
+        <label for="name">Full Name:</label>
+        <input type="text" id="name" value="Name"></input><br></br>
+        <label for="netID">netID:</label>
+        <input type="text" id="year" value="netID"></input><br></br>
+        <label for="year">Class Year:</label>
+        <input type="text" id="year" value="Class Year"></input><br></br>
+        <label for="major">Major:</label>
+        <input type="text" id="major" value="Major"></input><br></br>
+        <input type="submit" id="submit" value="Submit"></input><br></br>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,78 +1,17 @@
-//import React from "react";
-import React, { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import ReactDOM from "react-dom" ;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-import logo from './SWE_Logo_4C.png';
-
-//const element =<h1>Hello World</h1>;
-//ReactDOM. render(element, document. getElementById("root"));
-
-function Header(){
-          
-    return(     
-        <img src={logo} alt='logo' />,
-        <h1>SWE Attendence Form</h1>    
-    );
-}
-
-//export default Header;
-
-
-function AttendanceForm() {
-  const [name, setName] = useState('');
-  const [netID, setNetID] = useState('');
-  const [classYear, setClassYear] = useState('');
-  const [major, setMajor] = useState('');
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Write code to submit attendance data to backend API
-    // and update attendance.csv file
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input 
-          type="text" 
-          id="name" 
-          value={name} 
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="netID">NetID:</label>
-        <input 
-          type="text" 
-          id="netID" 
-          value={netID} 
-          onChange={(event) => setNetID(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="classYear">Class Year:</label>
-        <input 
-          type="text" 
-          id="classYear" 
-          value={classYear} 
-          onChange={(event) => setClassYear(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="major">Major:</label>
-        <input 
-          type="text" 
-          id="major" 
-          value={major} 
-          onChange={(event) => setMajor(event.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-export default AttendanceForm;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
