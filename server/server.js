0ce3d5adfaee8eb57f9e8b4728894f845a113256
Yuening Huang <yueninghuang962@gmail.com>
2023-03-25 21:57:26 -0400

    backend not work

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.post('/attendence', (req, res) => {
+    const { name, netID, year, major } = req.body;
+    const filename = 'sweAttendence.csv';
+    let alreadyExists = false;
+    let updated = false;
+    let count = 0;
+
+    // check if user already exists in the CSV file
+    fs.createReadStream(filename)
+        .pipe(csv())
+        .on('data', (row) => {
+            if (row.name === name && row.netID === netID) {
+                alreadyExists = true;
+                count = parseInt(row.attendance) || 0;
+            }
+        })
+        .on('end', () => {
+            if (alreadyExists) {
+                // if user already exists, increase cumulative number of attendances by 1
+                count++;
+                const rows = [];
+                fs.createReadStream(filename)
+                    .pipe(csv())
+                    .on('data', (row) => {
+                        if (row.name === name && row.netID === netID) {
+                            rows.push({ ...row, attendance: count });
+                        } else {
+                            rows.push(row);
+                        }
+                    })
+                    .on('end', () => {
+                        fs.writeFileSync(filename, '');
+                        rows.forEach((row) => {
+                            fs.appendFileSync(filename, '${row.name},${row.netID},${row.year},${row.major},${row.attendance}\n');
+                        });
+                        res.send({ success: true, message: 'User attendance updated' });
+                    });
+            } else {
+                // if user does not exist, add a new row to the CSV file
+                const newRecord = '${name},${netID},${year},${major},1\n';
+                fs.appendFileSync(filename, newRecord);
+                res.send({ success: true, message: 'New user attendance added' });
+            }
+        });
+});
+
+app.listen(port, () => {
+    console.log('Server listening at http://localhost:${port}');
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './SWE_Logo_4C.png';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function MyComponent() {
+  const [name, setName] = useState('');
+  const [netID, setNetID] = useState('');
+  const [year, setYear] = useState('');
+  const [major, setMajor] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('/attendence', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, netID, year, major })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // handle the response from the server here
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
   return (
     <div class ='attendence_form'>
       <img src={logo} alt='SWE logo' style={{width: '200px'}} />
       <h1>SWE Attendence Form</h1>
       <h4>For new members, please fill out the all information. </h4>
       <h4>For returning members, please only fill out name and netID.</h4>
-      <form class = 'form'>
-        <label for="name">Full Name:</label>
-        <input type="text" id="name" value="Name"></input><br></br>
-        <label for="netID">netID:</label>
-        <input type="text" id="year" value="netID"></input><br></br>
-        <label for="year">Class Year:</label>
-        <input type="text" id="year" value="Class Year"></input><br></br>
-        <label for="major">Major:</label>
-        <input type="text" id="major" value="Major"></input><br></br>
+      <form class = 'form' onSubmit={handleSubmit}>
+        <label htmlFor="name">Full Name:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}></input><br></br>
+        <label htmlFor="netID">netID:</label>
+        <input type="text" id="netID" value={netID} onChange={(e) => setNetID(e.target.value)}></input><br></br>
+        <label htmlFor="year">Class Year:</label>
+        <input type="text" id="year" value={year} onChange={(e) => setYear(e.target.value)}></input><br></br>
+        <label htmlFor="major">Major:</label>
+        <input type="text" id="major" value={major} onChange={(e) => setMajor(e.target.value)}></input><br></br>
         <input type="submit" id="submit" value="Submit"></input><br></br>
       </form>
     </div>
@@ -25,28 +49,3 @@ function MyComponent() {
 }
 
 export default MyComponent;
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
